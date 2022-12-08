45035d91ca0fd9914d76c86ded8a9d800ae9af59
Nikita Hoffman <hoffmanmyst@gmail.com>
2022-12-08 18:01:42 +0100

    config: configuration files added

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ['.eslintrc.js'],
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+
+  extends: ['@react-native-community', 'eslint-config-prettier', 'prettier'],
+  plugins: ['prettier', '@typescript-eslint', 'import'],
+
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-interface': 'warn',
+    '@typescript-eslint/no-shadow': 'off',
+    'max-len': ['error', { code: 75 }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': ['warn', { allow: ['error', 'info'] }],
+    'import/no-cycle': 'error',
+    'import/prefer-default-export': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
